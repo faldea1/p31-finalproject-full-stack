085a8b1133d546e7d5b304b1ec2a6de8cff2c7ca
faldea1 <faldea1@uc.cl>
2023-01-29 14:55:32 +0000

    Recap & Working Home Component

--- src/front/js/pages/new.js
+++ src/front/js/pages/new.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Form, Row, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 
 export const New = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div>
-			probando New...
-		</div>
-	);
+  return (
+    <Form>
+      <Container>
+        <Row>
+          <Nav>
+            <h3>New Orchard</h3>
+          </Nav>
+          	<div class="px-2.5">
+            	<div id="myalert" class="alert alert-danger px-3" role="alert">
+              		Some fields are missing
+            	</div>
+			</div>
+        </Row>
+      </Container>
+    </Form>
+  );
 };
